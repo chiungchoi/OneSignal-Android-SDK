71726f64ca60cbb155255015eb8c0ddbefd52974
Nan <nanftw@gmail.com>
2021-07-28 15:49:46 -0700

    Add unit test testIAMLifecycleEventsFlow

--- OneSignalSDK/onesignal/src/main/java/com/onesignal/OSInAppMessageController.java
+++ OneSignalSDK/onesignal/src/main/java/com/onesignal/OSInAppMessageController.java
@@ -391,6 +391,19 @@ class OSInAppMessageController extends OSBackgroundManager implements OSDynamicT
         logInAppMessagePreviewActions(action);
     }
 
+    /* IAM Lifecycle */
+    void onMessageWillDisplay(@NonNull final OSInAppMessageInternal message) {
+    }
+
+    void onMessageDidDisplay(@NonNull final OSInAppMessageInternal message) {
+    }
+
+    void onMessageWillDismiss(@NonNull final OSInAppMessageInternal message) {
+    }
+
+    void onMessageDidDismiss(@NonNull final OSInAppMessageInternal message) {
+    }
+
     private void logInAppMessagePreviewActions(final OSInAppMessageAction action) {
         if (action.getTags() != null)
             OneSignal.onesignalLog(OneSignal.LOG_LEVEL.DEBUG, "Tags detected inside of the action click payload, ignoring because action came from IAM preview:: " + action.getTags().toString());
